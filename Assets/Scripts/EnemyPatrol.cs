a11d1e8daba5a441fa11957fcceb846f1096c828
Nicholas Burnam <nickburnam789@gmail.com>
2020-10-28 14:11:02 -0700

    Added Wall Detection Patrol

--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -6,6 +6,9 @@ public class EnemyPatrol : MonoBehaviour
 {
     public float speed;
     private bool movingRight = true;
+    private bool hitWall = false;
+    public float checkRadius = 0.2f;
+    public LayerMask groundLayer;
     public Transform groundDetection;
 
     // Update is called once per frame
@@ -14,10 +17,29 @@ public class EnemyPatrol : MonoBehaviour
         transform.Translate(Vector2.right * speed * Time.deltaTime);
 
         RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 2f);
+        hitWall = Physics2D.OverlapCircle(groundDetection.position, checkRadius, groundLayer);
 
-        if(groundInfo.collider == false)
+        // Handle movement on floating platform
+        //
+        if (groundInfo.collider == false)
         {
-            if(movingRight == true)
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
+
+        // Handle movement on platform with walls
+        //
+        if (hitWall)
+        {
+            if (movingRight)
             {
                 transform.eulerAngles = new Vector3(0, -180, 0);
                 movingRight = false;
