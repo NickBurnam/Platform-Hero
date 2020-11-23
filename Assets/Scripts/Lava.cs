076dd578b821586a90d2b54779769c638e847142
NickBurnam <nickburnam789@gmail.com>
2020-11-23 01:35:29 -0800

    LAVA

---
+++ Assets/Scripts/Lava.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerHealth playerHealth = collision.GetComponent<PlayerHealth>();
+        if(playerHealth != null)
+        {
+            playerHealth.TakeDamage(1000);
+        }
+    }
+}
