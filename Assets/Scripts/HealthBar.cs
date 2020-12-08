208c8cb5f31fb8afe4d253261a955b7eae3d3ff9
NickBurnam <nickburnam789@gmail.com>
2020-12-07 20:00:40 -0800

    Fixed Health Bar position
    added ability to offset the healthbar for enemies

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -6,6 +6,12 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
+    public Vector3 offset = new Vector3(0f, 1f, 0f);
+
+    private void Update()
+    {
+        slider.transform.position = Camera.main.WorldToScreenPoint(transform.parent.position + offset);
+    }
 
     public void SetMaxHP(int maxHP)
     {
